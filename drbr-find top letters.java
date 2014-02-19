466f43b70b484a2bdcf5b836db9ca4a5fa874090
ssaatt <524706520@qq.com>
2014-02-18 19:04:21 -0500

    Update drbr-find top letters.java

--- drbr-find top letters.java
+++ drbr-find top letters.java
@@ -1,4 +1,15 @@
 /*
+面试时间: 6: 03-6:47
+Share Google Doc: 6:10.
+Clear Questions (input & output) : 6:10-6:18/6:20
+Time for coding:6:20-6:42
+问题: 
+1. 不需要sort, 时间复杂了, 只要keep max就好. 而且在加入的时候就能keep max, 不用one pass. 
+2. 这么简单个题, 写这么久!!!!
+*/
+
+
+/*
 Input: thiisiisgoodd
 Function F(input)
 output: i i o d
